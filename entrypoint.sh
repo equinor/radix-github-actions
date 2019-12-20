38d01d9722dc3b0eac3b2e7a9e6184624fca7830
ingeknudsen <iknu@equinor.com>
2019-12-20 20:44:29 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sh -c "echo $1 | /app/rx --token-stdin $2 $3 $4 $5 $6 $7 $8 $9 $10 $11 $12"
+sh -c "echo ${1} | /app/rx --token-stdin ${2} ${3} ${4} ${5} ${6} ${7} ${8} ${9} ${10} ${11} ${12}"
