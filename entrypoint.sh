c42c3de1bc1a6af582f30020957e9d9a36a3ae24
INGE KNUDSEN <ingeknudsen@INGEs-MacBook-Pro.local>
2019-12-23 12:33:10 +0100

    Take token from environment variable

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sh -c "echo ${1} | rx --token-stdin --from-config ${2} ${3} ${4} ${5} ${6} ${7} ${8} ${9} ${10} ${11} ${12}"
+sh -c "echo ${1} | rx --token-environment --from-config ${2} ${3} ${4} ${5} ${6} ${7} ${8} ${9} ${10} ${11} ${12}"
