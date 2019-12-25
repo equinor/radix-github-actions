19eba12d0f84e8385427cb7d8dac22eaec4ed8e1
ingeknudsen <iknu@equinor.com>
2019-12-25 10:16:28 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sh -c "echo ${1} | rx --token-environment --from-config ${1} ${2} ${3} ${4} ${5} ${6} ${7} ${8} ${9} ${10} ${11}"
+sh -c "echo ${1} | rx --token-environment --from-config ${1} ${2} ${3} ${4} ${5} ${6} ${7} ${8} ${9} ${10} ${11} ${12} ${13} ${14}"
