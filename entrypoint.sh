a72b5a13c5d319e8504b8aeff39b7a843605dddc
INGE KNUDSEN <ingeknudsen@INGEs-MacBook-Pro.local>
2020-01-03 19:45:53 +0100

    Another attempt to pass result

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-sh -c "rx --token-environment --from-config $*"
+sh -c "rx --token-environment --from-config $*" 2>&1 | tee result.txt
+result=$(cat result.txt)
+rm -f result.txt
+
+echo ::set-output name=result::$result
