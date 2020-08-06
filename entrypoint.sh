5d26b917bd055fffbbf08355322a9d6cf074913b
INGE KNUDSEN <ingeknudsen@INGEs-MacBook-Pro.local>
2020-08-06 13:35:32 +0200

    Added error handling

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,8 @@ sh -c "rx --token-environment --await-reconcile $*" 2>&1 | tee result.txt
 result=$(cat result.txt)
 rm -f result.txt
 
+if [[ $result == *"Error"* ]]; then
+    exit 1
+fi
+
 echo ::set-output name=result::$result
