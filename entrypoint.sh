f1cf42d845c4218d5b92a708973d7bcdab1627da
INGE KNUDSEN <ingeknudsen@INGEs-MacBook-Pro.local>
2020-08-06 15:19:12 +0200

    Test for the full error length

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@ sh -c "rx --token-environment --await-reconcile $*" 2>&1 | tee result.txt
 result=$(cat result.txt)
 rm -f result.txt
 
-case "$result" in *"Error"*)
+case "$result" in *"Error: Radix CLI executed with error"*)
     exit 1
     ;;
 esac
