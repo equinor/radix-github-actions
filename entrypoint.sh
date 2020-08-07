fa7085f8b42b22c4cd1755b658ceaadcc77777b4
ingeknudsen <iknu@equinor.com>
2020-08-07 15:30:04 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ set -e
 sh -c "rx --token-environment --await-reconcile $*" 2>&1 | tee result.txt
 
 echo "Obtain exit code"
-echo "Exit code is $PIPESTATUS"
+echo "Exit code is ${PIPESTATUS}"
 EXIT_CODE=${PIPESTATUS[0]}
 echo "Obtained exit code"
 
