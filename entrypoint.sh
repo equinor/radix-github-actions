0a4005c847e4c4f17295a3b2c1924e723afe3f56
ingeknudsen <iknu@equinor.com>
2021-07-14 09:28:19 +0200

    Ra 1523 return error (#2)
    * Added error handling
    
    * Added logging
    
    * Added logging
    
    * Yet another logging line
    
    * Should it be inside "
    
    * Print result
    
    * See if we get a better effect with regexp
    
    * Try with case
    
    * Remove log lines
    
    * Test for the full error length
    
    * Demonstrate effect
    
    * Return to full error
    
    Co-authored-by: INGE KNUDSEN <ingeknudsen@INGEs-MacBook-Pro.local>

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,9 @@ sh -c "rx --token-environment --await-reconcile $*" 2>&1 | tee result.txt
 result=$(cat result.txt)
 rm -f result.txt
 
+case "$result" in *"Error: Radix CLI executed with error"*)
+    exit 1
+    ;;
+esac
+
 echo ::set-output name=result::$result
