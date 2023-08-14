24af78a1ad3ea41026663c2b0f9a349a8b446054
Sergey Smolnikov <ssmol@equinor.com>
2023-08-14 12:49:06 +0200

    Specify Radix CLI explicitly to avoid use of not tested release

--- install_radix_cli.sh
+++ install_radix_cli.sh
@@ -1,20 +1,10 @@
 #!/bin/sh
-if [ "$1" != "" ]
-then
-  headers=$'--header \'authorization: Bearer '$1''\'''
-else
-  headers=""
-fi
+VERSION="1.7.4"
 
 # this action may be run in consecutive steps in same workflow. To avoid duplicate downloads, we check
 # if the rx cli tar file already exists
 ls radix-cli_*_Linux_x86_64.tar.gz >/dev/null 2>&1 || {
     apk add --update curl
-    VERSION=$(
-      curl --silent ${headers} https://api.github.com/repos/equinor/radix-cli/releases/latest |
-          grep '"tag_name":' |
-          sed -E 's/.*"v([^"]+)".*/\1/'
-    )
     wget https://github.com/equinor/radix-cli/releases/download/v${VERSION}/radix-cli_${VERSION}_Linux_x86_64.tar.gz
   }
 tar zxvf $(readlink -f radix-cli_*_Linux_x86_64.tar.gz)
