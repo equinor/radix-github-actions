61c8e248b324c23ae5c1052c90454ea9db864241
Sergey Smolnikov <ssmtor@gmail.com>
2023-08-21 15:43:08 +0200

    removed error analysis for rx run, using only exit code

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,3 @@
-/install_radix_cli.sh "$github_token"
-sh -c "rx --token-environment --await-reconcile $*" 2>&1 | tee result.txt
-result=$(cat result.txt)
+/install_radix_cli.sh
 
-case "$result" in *"Error: Radix CLI executed with error"*)
-    exit 1
-    ;;
-esac
-
-echo "result<<EOF" >> $GITHUB_OUTPUT
-cat result.txt >> $GITHUB_OUTPUT
-printf "\nEOF" >> $GITHUB_OUTPUT
-rm -f result.txt
+rx --token-environment $* 2>&1
