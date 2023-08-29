370d1f57eb5917cffe054bc3888c916cf0165406
Sergey Smolnikov <ssmtor@gmail.com>
2023-08-29 15:48:08 +0200

    Printed output to GITHUB_OUTPUT (#14)
    * Printed output to GITHUB_OUTPUT
    
    * Added diagnostics
    
    * Added validation of args
    
    * Added pipefail
    
    * Print exit code
    
    * Hide tar output

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,32 @@
 /install_radix_cli.sh
 
-if [[ "$*" == *"--token-environment"* ]]; then
-    rx $* 2>&1
-else
-    rx $* --token-environment 2>&1
+if [[ "$APP_SERVICE_ACCOUNT_TOKEN" == "" ]]; then
+  echo "no env-var APP_SERVICE_ACCOUNT_TOKEN, exiting"
+  exit 1
 fi
+
+set -o pipefail
+
+if [[ "$*" == *"--token-environment"* ]]; then
+  sh -c "rx $* 2>&1" | tee result.txt
+else
+  extended_options=" $* --token-environment "
+  sh -c "rx $extended_options 2>&1" | tee result.txt
+fi
+
+exit_code=$?
+
+result=$(cat result.txt)
+
+if [[ "$GITHUB_OUTPUT" == "" ]]; then
+  echo "no env-var GITHUB_OUTPUT, print result to stdout"
+  cat result.txt
+else
+  echo "result<<EOF" >> $GITHUB_OUTPUT
+  cat result.txt >> $GITHUB_OUTPUT
+  printf "\nEOF" >> $GITHUB_OUTPUT
+  rm -f result.txt
+fi
+
+echo "exit code: $exit_code"
+exit $exit_code
