40275984f636ed60861c3d148aaf07c1ee2a4b5f
Sergey Smolnikov <ssmol@equinor.com>
2023-08-29 14:55:53 +0200

    Printed output to GITHUB_OUTPUT

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,23 @@
 /install_radix_cli.sh
 
 if [[ "$*" == *"--token-environment"* ]]; then
-    rx $* 2>&1
+  sh -c "rx $* 2>&1" > result.txt
 else
-    rx $* --token-environment 2>&1
+  extended_options="--token-environment $*"
+  sh -c "rx $extended_options 2>&1" > result.txt
 fi
+exit_code=$?
+
+result=$(cat result.txt)
+
+if [[ "$GITHUB_OUTPUT" == "" ]]; then
+  cat result.txt
+  exit $exit_code
+fi
+
+echo "result<<EOF" >> $GITHUB_OUTPUT
+cat result.txt >> $GITHUB_OUTPUT
+printf "\nEOF" >> $GITHUB_OUTPUT
+rm -f result.txt
+
+exit $exit_code
