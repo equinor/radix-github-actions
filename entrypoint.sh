80ea7a495fedef391874f59cb9f2e0132e922cd8
Nils Gustav Stråbø <nst@equinor.com>
2024-04-03 08:50:55 +0200

    revert tests

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,21 +4,11 @@ if [[ "$APP_SERVICE_ACCOUNT_TOKEN" == "" ]]; then
 fi
 
 set -o pipefail
-printf "print args--\n"
-printf "$*"
-printf $#
-echo "$*"
-echo "quoted"
-echo "$@"
-printf "--end print args\n"
 
 if [[ "$*" == *"--token-environment"* ]]; then
-  echo "contains --token-environment"
   sh -c "rx $* 2>&1" | tee result.txt
 else
-  echo "does not contain --token-environment"
   extended_options=" $* --token-environment "
-  echo "$extended_options"
   sh -c "rx $extended_options 2>&1" | tee result.txt
 fi
 
