2426e09a0f20eae13d91510c39f86d2190e2a779
Richard87 <richard.hagen@gmail.com>
2025-04-23 12:38:41 +0200

    fix: fail on removed args parameter

--- src/utils.ts
+++ src/utils.ts
@@ -76,8 +76,9 @@ export async function installRx(version: string, filename: string) {
 }
 
 export async function getOptions() {
-    const args = core.getInput("args");
+    const args = process.argv.join("");
     if (args) {
+        console.debug("args", args);
         throw new Error("args is removed and not supported. Either pin your action to v1, or upgrade your Github Workflow file to use the new configuration. See the README for more details https://github.com/equinor/radix-github-actions");
     }
 
