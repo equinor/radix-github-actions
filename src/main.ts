80239aba6bf9cd7fd49e1737a3845ce31d7610e7
Nils Gustav Stråbø <65334626+nilsgstrabo@users.noreply.github.com>
2026-07-30 13:31:06 +0200

    revert test

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function run() {
     const { version, authenticate, azureClientId, azureClientSecret, githubAuth, githubToken } = await getOptions();
 
     const rxUrl = await getUrl(version);
-    console.log("Setup RX with config (test)", {
+    console.log("Setup RX with config", {
         authenticate,
         version,
         rxUrl,
